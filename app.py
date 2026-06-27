9993b4974c4ed645e9629d76de221686699cb56f
LUISKA777 <lr4658821@gmail.com>
2026-06-27 09:28:10 -0600

    Restaurando proyecto desde respaldo ZIP

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
 import os
 import requests as http
-from datetime import date, timedelta, datetime, time
+from datetime import date, timedelta, datetime, time 
 import pytz
 from urllib.parse import unquote
 from werkzeug.security import generate_password_hash, check_password_hash
